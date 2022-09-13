813416ac1fbb0be5c1ce715279eb49f19e2fe0c8
Crisano97 <97keb6@gmail.com>
2022-09-13 17:28:51 +0200

    changed faker description length

--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -17,7 +17,7 @@ class ComicsTableSeeder extends Seeder
         for($i = 0; $i < 20; $i++){
             $newComic = new Comic();
             $newComic->title = $faker->unique()->realText(35);
-            $newComic->description = $faker->realText(35);
+            $newComic->description = $faker->realText();
             $newComic->thumb = $faker->imageUrl(500, 300, "comic");
             $newComic->price = $faker->randomFloat(2, 4, 15);
             $newComic->series = $faker->realText(15);
