fd69c7744f4a33ecd6bd2a2505094edb776759f2
Crisano97 <97keb6@gmail.com>
2022-09-13 17:15:52 +0200

    using slug instead of id to show single comic

--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -3,6 +3,7 @@
 use App\Models\Comic;
 use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -15,14 +16,14 @@ class ComicsTableSeeder extends Seeder
     {
         for($i = 0; $i < 20; $i++){
             $newComic = new Comic();
-            $newComic->title = $faker->realText(35);
+            $newComic->title = $faker->unique()->realText(35);
             $newComic->description = $faker->realText(35);
             $newComic->thumb = $faker->imageUrl(500, 300, "comic");
             $newComic->price = $faker->randomFloat(2, 4, 15);
             $newComic->series = $faker->realText(15);
             $newComic->sale_date = $faker->date();
             $newComic->type = $faker->realText(15);
-
+            $newComic->slug = Str::slug($newComic->title, '-');
             $newComic->save();
         }
     }
