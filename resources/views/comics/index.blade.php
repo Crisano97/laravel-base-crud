365241962c87e75d7fe490dfaae0fc830ab08be1
Crisano97 <97keb6@gmail.com>
2022-09-13 15:31:33 +0200

    obtaining all comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,6 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
+                @dump($comics)
                 <h3>main content</h3>
             </div>
         </div>
