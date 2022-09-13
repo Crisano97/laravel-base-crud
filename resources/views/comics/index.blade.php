17ee3734d63a62ca169601a6a1b8b05c1739079f
Crisano97 <97keb6@gmail.com>
2022-09-13 15:05:46 +0200

    laravel base crud, blades, model and controller creation

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3>main content</h3>
+            </div>
+        </div>
+    </div>
+@endsection
